c64003e74e8cbbec5a56c2b8073f20dcf27295ef
Audrey-Wangui <audreywangui1@gmail.com>
2026-03-03 13:55:10 +0300

    updated code

--- assignment5.js
+++ assignment5.js
@@ -6,8 +6,8 @@ while (n <= 50) {
     else if(n %5 == 0) {
         console.log("Buzz")
     }
-    else if((n % 5) == 0 && (n % 3 == 0)) {
-        console.log("FizzBuzz")
+    else if(n %3 == 0) {
+        console.log("Fizz")
     }
     else {
         console.log(n)
